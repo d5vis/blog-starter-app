908b0d2a187cc221c2c369a2fce4504ed119ecc8
d5vis <davisjbanks@gmail.com>
2022-10-26 13:05:54 -0700

    installed firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXXTDPo6ISu0wBN0Tq6cPGj_kq97HlA9g",
+
+  authDomain: "blog-d5vis.firebaseapp.com",
+
+  projectId: "blog-d5vis",
+
+  storageBucket: "blog-d5vis.appspot.com",
+
+  messagingSenderId: "309777248893",
+
+  appId: "1:309777248893:web:65ff7eb714d4bc3f64b714",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
